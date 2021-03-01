395dc804f20b64d03f066a51bb2e40a968d00c0a
JViii <john2000@live.com.pt>
2021-03-01 11:44:40 +0000

    fix identation on MCRunnable.java

--- labs/lab2/MCRunnable.java
+++ labs/lab2/MCRunnable.java
@@ -10,12 +10,12 @@ public class MCRunnable implements Runnable {
   int servicePort, mcPort; 
   DatagramSocket socket;
 
-  public MCRunnable(InetAddress serviceIP, int servicePort, InetAddress mcAddress, int mcPort) throws IOException {
-		this.socket = new DatagramSocket();
-		this.serviceIP= serviceIP;
+  public MCRunnable(InetAddress serviceIP, int servicePort, InetAddress mcAddress, int mcPort) throws IOException 
+    this.socket = new DatagramSocket();
+    this.serviceIP= serviceIP;
     this.servicePort = servicePort;
-    this.mcAddress = mcAddress;
-		this.mcPort = mcPort;
+    this.mcAddress = mcAddress
+    this.mcPort = mcPort;
   }
 
   public void run() {
@@ -25,16 +25,16 @@ public class MCRunnable implements Runnable {
     catch(IOException e){
       throw new RuntimeException(e);
     }
-	}
+  }
 
-	public void multicast() throws IOException{
+  public void multicast() throws IOException{
     byte[] buf = new byte[1024];
     buf = new String(serviceIP.getHostAddress() + " " + this.servicePort).getBytes();
 
     DatagramPacket packet = new DatagramPacket(buf,buf.length,this.mcAddress,this.mcPort);
     socket.send(packet);
     System.out.println("multicast: " + this.mcAddress.getHostAddress() + " " + this.mcPort + ": " + this.serviceIP.getHostAddress() + " " + this.servicePort);
-	}
+  }
 }
 
 
